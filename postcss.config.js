c1b6f655ba8e1926ff8363d24073ac1da849ea14
Shaelah Zebato <nadiakareneshaelah@gmail.com>
2025-07-09 20:44:40 +0000

    Fix: Mise à jour de la configuration de PostCSS pour Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
