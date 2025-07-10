d0b4b692f9962df8795ee72c0032a8c600f84ed3
Shaelah Zebato <nadiakareneshaelah@gmail.com>
2025-07-10 09:01:10 +0000

    Fix: Configuration de Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="bg-cream min-h-screen flex items-center justify-center">
@@ -11,7 +13,7 @@ export default function Home() {
         <div className="mt-8">
           <div className="text-4xl animate-float-1">🧁</div>
         </div>
-        <button className="text-cream border-chocolat border bg-rose-bonbon px-4 py-2 rounded-2xl mt-8">Dyaaa !</button>
+        <Link href={"/test"} className="text-cream border-chocolat border bg-rose-bonbon px-4 py-2 rounded-2xl mt-8">Dyaaa !</Link>
       </div>
     </div>
   );
